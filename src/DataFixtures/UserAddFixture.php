aceb9ce1f8483811c1424d89f3c378d53b5be413
Evgenii Ryshkov <eryshkov@gmail.com>
2019-05-22 13:41:56 +0300

    RolesCreateFixture.php added

--- src/DataFixtures/UserAddFixture.php
+++ src/DataFixtures/UserAddFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,10 @@ class UserAddFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        
+        $user->setEmail('admin@test.mac');
+        $manager->persist($user);
 
         $manager->flush();
     }
